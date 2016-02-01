3077c4337c3f680ce68bb03bec80f11e8d782a03
Jairo andres castañeda <andresbbx@gmail.com>
2016-02-01 17:56:21 -0500

    acomodo proyecto
    proyecto acomodado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+         $this->app->bind('path.public', function() {
+            return base_path().'/public_html';
+        });
     }
 
     /**
